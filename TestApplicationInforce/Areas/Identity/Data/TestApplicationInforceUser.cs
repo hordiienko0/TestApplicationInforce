c741b129c218f4d90deae55f4c0c5f6c09c84a0b
hordienko3000@outlook.com <hordienko3000@outlook.com>
2022-07-13 23:55:21 +0300

    Add project files.

---
+++ TestApplicationInforce/Areas/Identity/Data/TestApplicationInforceUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace TestApplicationInforce.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the TestApplicationInforceUser class
+public class TestApplicationInforceUser : IdentityUser
+{
+}
+
